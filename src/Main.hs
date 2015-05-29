8cf7b7d9c107dfb96fb698f1779f5969f78e6e80
julianperrott <julian.perrott@gmail.com>
2015-05-29 14:33:08 -0700

    2048 Completed

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,6 @@
-import Codewars.EscapeTheMines
+import Codewars.Move2048
 
---main=test
+main=test
 
-import Data.List
-
-pad:: String -> String
-pad s = replicate (4- (length s)) ' ' ++s
-
---main = map putStrLn $ map (\x -> intercalate " " $ map (\y -> pad $ show (y*x)) [1..12])[1..12] 
 
 
